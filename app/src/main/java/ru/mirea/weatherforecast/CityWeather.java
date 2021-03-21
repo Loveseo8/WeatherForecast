7088f9c1895f73550d11b55d76490571878d8fb4
User <aaredov@bk.ru>
2021-03-21 13:26:05 -0200

    graph

--- app/src/main/java/ru/mirea/weatherforecast/CityWeather.java
+++ app/src/main/java/ru/mirea/weatherforecast/CityWeather.java
@@ -2,9 +2,13 @@ package ru.mirea.weatherforecast;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,7 +38,12 @@ public class CityWeather extends AppCompatActivity {
 
     TextView city;
     String selected;
+    int selected1;
     LineChart lineChart;
+    Button btn;
+    List<String> cities = null;
+    List<String> tear = null;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +55,6 @@ public class CityWeather extends AppCompatActivity {
 
         city = findViewById(R.id.city_nmae);
         city.setText(selected);
-        List<String> cities = null;
         try {
             cities = jsonParsing(selected);
         } catch (JSONException e) {
@@ -58,10 +66,11 @@ public class CityWeather extends AppCompatActivity {
         lineChart = (LineChart) findViewById(R.id.linechart);
 
         ArrayList<Entry> entries = new ArrayList<>();
-        // Массив координат точек
-        for(int m = 0; m < cities.size(); m++) {
+        for (int m = 0; m < cities.size(); m++) {
             entries.add(new Entry(m + 1, (float) Double.parseDouble(cities.get(m))));
         }
+
+
 
         LineDataSet dataset = new LineDataSet(entries, "Температура за год");
 
@@ -70,7 +79,226 @@ public class CityWeather extends AppCompatActivity {
 
         lineChart.invalidate();
 
+        spinner = findViewById(R.id.year);
+        String[] years = {"1", "2", "3", "4", "5", "6 ", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, years);
+        spinner.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        btn = findViewById(R.id.btn);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selected1 = position + 1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+            btn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    ArrayList<Entry> entries = new ArrayList<>();
+
+                    for (int m = getStart(selected1); m < getEnd(selected1); m++) {
+                        Log.d("TAGA", String.valueOf(selected1));
+                        entries.add(new Entry(m + 1, (float) Double.parseDouble(cities.get(m))));
+                    }
+
+                    LineDataSet dataset = new LineDataSet(entries, "Температура");
+
+                    LineData data = new LineData(dataset);
+                    lineChart.setData(data);
+
+                    lineChart.invalidate();
+                }
+            });
+
+        }
+
+         int getStart(int selected){
+
+        int start = 0;
+
+        switch (selected) {
+
+            case 1:
+                start = 0;
+                break;
+
+            case 2:
+                start = 365;
+                break;
+            case 3:
+                start = 730;
+                break;
+            case 4:
+                start = 1095;
+                break;
+            case 5:
+                start = 1460;
+                break;
+            case 6:
+                start = 1825;
+                break;
+            case 7:
+                start = 2190;
+                break;
+
+            case 8:
+                start = 2555;
+                break;
+
+            case 9:
+                start = 2920;
+                break;
+
+
+            case 11:
+                start = 3650;
+                break;
+
+            case 12:
+                start = 4015;
+                break;
+
+            case 13:
+                start = 4380;
+                break;
+
+            case 14:
+                start = 4745;
+                break;
+
+            case 15:
+                start = 5110;
+                break;
+
+            case 16:
+                start = 5475;
+                break;
+
+            case 17:
+                start = 5840;
+                break;
+
+            case 18:
+                start = 6205;
+                break;
+
+            case 19:
+                start = 6570;
+                break;
+
+            case 20:
+                start = 6935;
+                break;
+
+
+            default:
+                break;
+        }
+
+        return  start;
+
     }
+
+    int getEnd(int selected){
+
+
+        int end = 0;
+
+        switch (selected) {
+
+            case 1:
+                end = 365;
+                break;
+
+            case 2:
+                end = 730;
+                break;
+            case 3:
+                end = 1095;
+                break;
+            case 4:
+                end = 1460;
+                break;
+            case 5:
+                end = 1825;
+                break;
+            case 6:
+                end = 2190;
+                break;
+            case 7:
+                end = 2555;
+                break;
+
+            case 8:
+                end = 2920;
+                break;
+
+            case 9:
+                end = 3285;
+                break;
+
+
+            case 11:
+                end = 4015;
+                break;
+
+            case 12:
+                end = 4380;
+                break;
+
+            case 13:
+                end = 4745;
+                break;
+
+            case 14:
+                end = 5110;
+                break;
+
+            case 15:
+                end = 5475;
+                break;
+
+            case 16:
+                end = 5840;
+                break;
+
+            case 17:
+                end = 6205;
+                break;
+
+            case 18:
+                end = 6570;
+                break;
+
+            case 19:
+                end = 6935;
+                break;
+
+            case 20:
+                end = 7300;
+                break;
+
+
+            default:
+                break;
+        }
+
+        return  end;
+
+    }
+
+
 
     List jsonParsing(String selected) throws JSONException, IOException {
 
